17d4c8bdf79707e2488566b167fb64043269ffdc
omerhalid <omerhalitcinar@edu.bme.hu>
2024-09-27 01:32:51 +0200

    Refactor OrderBook.cpp to use .hpp extension for included file

--- src/order_matching/OrderBook.cpp
+++ src/order_matching/OrderBook.cpp
@@ -3,17 +3,21 @@
 #include <queue>
 #include <vector>
 #include <functional>
+#include <unordered_map>
+
+// Alias for Order<double>
+using OrderDouble = Order<double>;
 
 // Comparison functor for buy orders (max-heap)
 struct BuyOrderCompare {
-    bool operator()(const Order<double>& lhs, const Order<double>& rhs) const {
+    bool operator()(const OrderDouble& lhs, const OrderDouble& rhs) const {
         return lhs.price < rhs.price; // Higher price has higher priority
     } // timestamp can be added here
 };
 
 // Comparison functor for sell orders (min-heap)
 struct SellOrderCompare {
-    bool operator()(const Order<double>& lhs, const Order<double>& rhs) const {
+    bool operator()(const OrderDouble& lhs, const OrderDouble& rhs) const {
         return lhs.price > rhs.price; // Lower price has higher priority
     }
 };
@@ -21,12 +25,13 @@ struct SellOrderCompare {
 class OrderBook
 {
 private:
-    std::priority_queue<Order<double>, std::vector<Order<double>>, BuyOrderCompare> buyOrders;
-    std::priority_queue<Order<double>, std::vector<Order<double>>, SellOrderCompare> sellOrders;
+    std::priority_queue<OrderDouble, std::vector<OrderDouble>, BuyOrderCompare> buyOrders;
+    std::priority_queue<OrderDouble, std::vector<OrderDouble>, SellOrderCompare> sellOrders;
 
-    std::unordered_map<int, Order<double>> orderMap;
+    std::unordered_map<int, OrderDouble> orderMap;
 
-    void removeFromHeap()
+    template<typename Compare>
+    void removeFromHeap(std::priority_queue<OrderDouble, std::vector<OrderDouble>, Compare> &heap, int id)
     {
         // Implementation for removing from heap
     }
@@ -34,7 +39,7 @@ private:
 public:
     void AddOrder(int id_, int quantity_, double price_, bool isBuyOrder_)
     {
-        Order<double> newOrder(id_, quantity_, price_, isBuyOrder_);
+        OrderDouble newOrder(id_, quantity_, price_, isBuyOrder_);
         orderMap[id_] = newOrder;
         
         if (isBuyOrder_)
@@ -44,6 +49,23 @@ public:
         else
         {
             sellOrders.push(newOrder);
+        }
+    }
+
+    void CancelOrder(int id)
+    {
+        if(orderMap.find(id) != orderMap.end())
+        {
+            OrderDouble cancelOrder = std::move(orderMap[id]);
+            orderMap.erase(id);
+            if (cancelOrder.isBuyOrder)
+            {
+                removeFromHeap(buyOrders, id);
+            }
+            else
+            {
+                removeFromHeap(sellOrders, id);
+            }
         }
     }
 };
