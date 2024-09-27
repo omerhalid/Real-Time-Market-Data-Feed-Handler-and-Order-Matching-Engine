445094f434b5d663bff208478c3ef3cb9dfc93d3
omerhalid <omerhalitcinar@edu.bme.hu>
2024-09-27 16:02:43 +0200

    Refactor build files and add progress tracking

--- build/CMakeFiles/3.29.2/CMakeRCCompiler.cmake
+++ build/CMakeFiles/3.29.2/CMakeRCCompiler.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_RC_COMPILER "rc")
+set(CMAKE_RC_COMPILER "C:/Users/katka/Downloads/winlibs-x86_64-posix-seh-gcc-14.2.0-llvm-18.1.8-mingw-w64ucrt-12.0.0-r1/mingw64/bin/windres.exe")
 set(CMAKE_RC_COMPILER_ARG1 "")
 set(CMAKE_RC_COMPILER_LOADED 1)
 set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .res)
+set(CMAKE_RC_OUTPUT_EXTENSION .obj)
 set(CMAKE_RC_COMPILER_ENV_VAR "RC")
